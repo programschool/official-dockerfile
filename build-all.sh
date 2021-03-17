e6ed88d2ed656117741afaeb17b498a06ae3254d
foxsir <838394225@qq.com>
2021-03-17 23:10:59 +0800

    update

--- build-all.sh
+++ build-all.sh
@@ -1,101 +1,89 @@
 #!/usr/bin/env bash
 
 cd C
-pscli . official/c:latest 2>&1 > /dev/null &
+pscli . official/c 2>&1 > /dev/null &
 cd ..
 cd C#
-pscli . official/c-sharp:latest 2>&1 > /dev/null &
+pscli . official/c-sharp 2>&1 > /dev/null &
 cd ..
 cd C++
-pscli . official/cpp:latest 2>&1 > /dev/null &
+pscli . official/cpp 2>&1 > /dev/null &
 cd ..
 cd CoffeeScript
-pscli . official/coffeescript:latest 2>&1 > /dev/null &
-cd ..
-cd CSS
-pscli . official/css:latest 2>&1 > /dev/null &
+pscli . official/coffeescript 2>&1 > /dev/null &
 cd ..
 cd Dart
-pscli . official/dart:latest 2>&1 > /dev/null &
+pscli . official/dart 2>&1 > /dev/null &
 cd ..
 cd Deno
-pscli . official/deno:latest 2>&1 > /dev/null &
+pscli . official/deno 2>&1 > /dev/null &
 cd ..
 cd Golang
-pscli . official/golang:latest 2>&1 > /dev/null &
+pscli . official/golang 2>&1 > /dev/null &
 cd ..
 cd Groovy
-pscli . official/groovy:latest 2>&1 > /dev/null &
-cd ..
-cd HTML
-pscli . official/html:latest 2>&1 > /dev/null &
+pscli . official/groovy 2>&1 > /dev/null &
 cd ..
 cd Java
-pscli . official/java:latest 2>&1 > /dev/null &
+pscli . official/java 2>&1 > /dev/null &
 cd ..
 cd Java8
-pscli . official/java8:latest 2>&1 > /dev/null &
-cd ..
-cd JavaScript
-pscli . official/javascript:latest 2>&1 > /dev/null &
+pscli . official/java8 2>&1 > /dev/null &
 cd ..
 cd Kotlin
-pscli . official/kotlin:latest 2>&1 > /dev/null &
+pscli . official/kotlin 2>&1 > /dev/null &
 cd ..
 cd Mariadb
-pscli . official/mariadb:latest 2>&1 > /dev/null &
+pscli . official/mariadb 2>&1 > /dev/null &
 cd ..
 cd Memcached
-pscli . official/memcached:latest 2>&1 > /dev/null &
+pscli . official/memcached 2>&1 > /dev/null &
 cd ..
 cd Mongo
-pscli . official/mongo:latest 2>&1 > /dev/null &
+pscli . official/mongo 2>&1 > /dev/null &
 cd ..
 cd Mysql
-pscli . official/mysql:latest 2>&1 > /dev/null &
+pscli . official/mysql 2>&1 > /dev/null &
 cd ..
 cd Nginx
-pscli . official/nginx:latest 2>&1 > /dev/null &
+pscli . official/nginx 2>&1 > /dev/null &
 cd ..
 cd Node
-pscli . official/node:latest 2>&1 > /dev/null &
+pscli . official/node 2>&1 > /dev/null &
 cd ..
 cd Objective-C
-pscli . official/objective-c:latest 2>&1 > /dev/null &
+pscli . official/objective-c 2>&1 > /dev/null &
 cd ..
 cd Perl
-pscli . official/perl:latest 2>&1 > /dev/null &
+pscli . official/perl 2>&1 > /dev/null &
 cd ..
 cd PHP
-pscli . official/php:latest 2>&1 > /dev/null &
+pscli . official/php 2>&1 > /dev/null &
 cd ..
 cd Postgres
-pscli . official/postgres:latest 2>&1 > /dev/null &
+pscli . official/postgres 2>&1 > /dev/null &
 cd ..
 cd Python
-pscli . official/python:latest 2>&1 > /dev/null &
+pscli . official/python 2>&1 > /dev/null &
 cd ..
 cd Redis
-pscli . official/redis:latest 2>&1 > /dev/null &
+pscli . official/redis 2>&1 > /dev/null &
 cd ..
 cd Ruby
-pscli . official/ruby:latest 2>&1 > /dev/null &
+pscli . official/ruby 2>&1 > /dev/null &
 cd ..
 cd Rust
-pscli . official/rust:latest 2>&1 > /dev/null &
+pscli . official/rust 2>&1 > /dev/null &
 cd ..
 cd Scala
-pscli . official/scala:latest 2>&1 > /dev/null &
-cd ..
-cd Shell
-pscli . official/shell:latest 2>&1 > /dev/null &
+pscli . official/scala 2>&1 > /dev/null &
 cd ..
 cd Swift
-pscli . official/swift:latest 2>&1 > /dev/null &
+pscli . official/swift 2>&1 > /dev/null &
 cd ..
 cd TypeScript
-pscli . official/typescript:latest 2>&1 > /dev/null &
+pscli . official/typescript 2>&1 > /dev/null &
 cd ..
 cd Ubuntu
-pscli . official/ubuntu:latest 2>&1 > /dev/null &
+pscli . official/ubuntu 2>&1 > /dev/null &
 cd ..
