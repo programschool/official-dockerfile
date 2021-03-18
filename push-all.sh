7000fae9eed85a22e67ad8cff8817cb69ec1a722
foxsir <838394225@qq.com>
2021-03-18 11:38:39 +0800

    add push script

---
+++ push-all.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+docker push boxlayer.com/official/ubuntu
+docker push boxlayer.com/official/typescript
+docker push boxlayer.com/official/swift
+docker push boxlayer.com/official/scala
+docker push boxlayer.com/official/rust
+docker push boxlayer.com/official/ruby
+docker push boxlayer.com/official/redis
+docker push boxlayer.com/official/python
+docker push boxlayer.com/official/postgres
+docker push boxlayer.com/official/php
+docker push boxlayer.com/official/perl
+docker push boxlayer.com/official/objective-c
+docker push boxlayer.com/official/node
+docker push boxlayer.com/official/nginx
+docker push boxlayer.com/official/mysql
+docker push boxlayer.com/official/mongo
+docker push boxlayer.com/official/memcached
+docker push boxlayer.com/official/mariadb
+docker push boxlayer.com/official/kotlin
+docker push boxlayer.com/official/java8
+docker push boxlayer.com/official/java
+docker push boxlayer.com/official/groovy
+docker push boxlayer.com/official/golang
+docker push boxlayer.com/official/deno
+docker push boxlayer.com/official/dart
+docker push boxlayer.com/official/coffeescript
+docker push boxlayer.com/official/cpp
+docker push boxlayer.com/official/c-sharp
+docker push boxlayer.com/official/c
